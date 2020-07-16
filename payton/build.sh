bde00f371968870518d2fcf922274d4774fd50c8
ゆうこスカイ ~#YuMi1621 <daffetyxd@gmail.com>
2020-07-15 22:27:40 -0300

    payton-shrp -> payton (twrp)

---
+++ payton/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Sync
+
+# Build
+telegram -M "TWRP - Moto X4: Building Recovery started"
+SYNC_START=$(date +"%s")
+
+. build/envsetup.sh
+lunch omni_payton-eng
+mka recoveryimage
+
+SYNC_END=$(date +"%s")
+SYNC_DIFF=$((SYNC_END - SYNC_START))
+telegram -M "TWRP - Moto X4: Building completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
+
+# Output
+ls out/target/product/payton/
+telegram -f out/target/product/payton/recovery.img "[SHRP Recovery] [Unofficial] - Moto X4"
