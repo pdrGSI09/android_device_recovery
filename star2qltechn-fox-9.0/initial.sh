742af1675c4390b668eee131d1ce5bd136b07ad9
renatohribeiro <renatohribeh@outlook.com>
2020-07-27 00:16:17 -0300

    Test

--- star2qltechn-fox-9.0/initial.sh
+++ star2qltechn-fox-9.0/initial.sh
@@ -9,10 +9,10 @@ sudo apt-get install default-jdk android-tools-adb bison build-essential curl fl
 sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
 sudo chmod a+rx /usr/local/bin/repo
 
-repo init --depth=1 -u https://gitlab.com/OrangeFox/Manifest.git -b fox_9.0
+repo init --depth=1 -u https://gitlab.com/OrangeFox/Manifest.git -b fox_8.1
 repo sync --no-repo-verify -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j`nproc`
 mkdir -p device/samsung/; cd device/samsung
-git clone https://github.com/klabit87/twrp_android_device_samsung_star2qltechn.git -b 9.0 star2qltechn; cd ../
+git clone https://github.com/klabit87/twrp_android_device_samsung_star2qltechn.git -b 8.1 star2qltechn; cd ../
 
 SYNC_END=$(date +"%s")
 SYNC_DIFF=$((SYNC_END - SYNC_START))
